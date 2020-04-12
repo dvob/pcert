53a377fe79324808a079c2119d1f847c1350b73f
David Schneider <dsbrng25b@gmail.com>
2020-04-12 22:23:10 +0200

    Add option for subject and defaulting for key usage

--- config.go
+++ config.go
@@ -2,6 +2,7 @@ package pcert
 
 import (
 	"crypto/x509"
+	"crypto/x509/pkix"
 	"fmt"
 	"net"
 	"net/url"
@@ -20,6 +21,7 @@ func BindFlags(fs *pflag.FlagSet, cert *x509.Certificate, prefix string) {
 	fs.Var(newSignAlgValue(&cert.SignatureAlgorithm), prefix+"sign-alg", "Signature Algorithm")
 	fs.Var(newTimeValue(&cert.NotBefore), prefix+"not-before", "Not valid before time in RFC3339 format")
 	fs.Var(newTimeValue(&cert.NotAfter), prefix+"not-after", "Not valid after time in RFC3339 format")
+	fs.Var(newSubjectValue(&cert.Subject), prefix+"subject", "Subject in the form '/C=CH/O=My Org/OU=My Team'")
 }
 
 func BindKeyFlags(fs *pflag.FlagSet, keyConfig *KeyConfig, prefix string) {
@@ -146,5 +148,67 @@ func (t *timeValue) Set(timeStr string) error {
 		return err
 	}
 	*t.value = parsedTime
+	return nil
+}
+
+type subjectValue struct {
+	value *pkix.Name
+}
+
+func newSubjectValue(subject *pkix.Name) *subjectValue {
+	return &subjectValue{
+		value: subject,
+	}
+}
+
+func (s *subjectValue) Type() string {
+	return "subject"
+}
+
+func (s *subjectValue) String() string {
+	return s.value.String()
+}
+
+func (s *subjectValue) Set(subject string) error {
+	return parseSubjectInto(subject, s.value)
+}
+
+func parseSubjectInto(subject string, target *pkix.Name) error {
+	for _, part := range strings.Split(subject, "/") {
+		if part == "" {
+			continue
+		}
+		parts := strings.SplitN(part, "=", 2)
+		if len(parts) != 2 {
+			return fmt.Errorf("failed to parse subject. could not split '%s'", part)
+		}
+		key := parts[0]
+		value := parts[1]
+
+		switch key {
+		case "C":
+			target.Country = append(target.Country, value)
+		case "O":
+			target.Organization = append(target.Organization, value)
+		case "OU":
+			target.OrganizationalUnit = append(target.OrganizationalUnit, value)
+		case "L":
+			target.Locality = append(target.Locality, value)
+		case "P":
+			target.Province = append(target.Province, value)
+		case "ST":
+			target.StreetAddress = append(target.StreetAddress, value)
+		case "STREET":
+			target.StreetAddress = append(target.StreetAddress, value)
+		case "POSTALCODE":
+			target.PostalCode = append(target.PostalCode, value)
+		case "SERIALNUMBER":
+			target.SerialNumber = target.SerialNumber
+		case "CN":
+			target.CommonName = value
+		default:
+			return fmt.Errorf("unknown field '%s'", key)
+		}
+	}
 	return nil
 }
--- default.go
+++ default.go
@@ -1,29 +1,50 @@
 package pcert
 
 import (
-	cryptorand "crypto/rand"
+	"crypto/rand"
 	"crypto/x509"
-	"crypto/x509/pkix"
 	"fmt"
 	"math/big"
-	"math/rand"
 	"time"
 )
 
 const (
-	DefaultValidityPeriod     = time.Hour * 24 * 90
-	DefaultSignatureAlgorithm = x509.SHA256WithRSA
+	DefaultValidityPeriod = time.Hour * 24 * 365
+	DefaultKeyUsage       = x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature
+)
+
+var (
+	defaultExtKeyUsage = []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth}
 )
 
 func DefaultWithName(name string, template *x509.Certificate) (*x509.Certificate, error) {
-	template.Subject = pkix.Name{CommonName: name}
-	Default(template)
+	if template.Subject.CommonName == "" {
+		template.Subject.CommonName = name
+	}
+	defaultTime(template)
+	defaultKeyUsage(template)
+	_ = defaultSerialNumber(template)
 	return template, nil
 }
 
-func Default(c *x509.Certificate) {
-	defaultSerialNumber(c)
+func defaultCA(c *x509.Certificate) {
+	c.KeyUsage |= x509.KeyUsageCertSign | x509.KeyUsageCRLSign
+	c.IsCA = true
+	c.BasicConstraintsValid = true
+}
 
+func defaultKeyUsage(c *x509.Certificate) {
+	if c.KeyUsage != 0 {
+		return
+	}
+	c.KeyUsage = DefaultKeyUsage
+
+	if len(c.ExtKeyUsage) == 0 {
+		c.ExtKeyUsage = defaultExtKeyUsage
+	}
+}
+
+func defaultTime(c *x509.Certificate) {
 	if c.NotBefore.IsZero() {
 		c.NotBefore = time.Now()
 	}
@@ -33,21 +54,12 @@ func Default(c *x509.Certificate) {
 	}
 }
 
-func defaultSerialNumber(c *x509.Certificate) {
-	if c.SerialNumber != nil {
-		return
-	}
-	maxuint64 := ^uint64(0)
-	maxint64 := int64(maxuint64 >> 1)
-	c.SerialNumber = big.NewInt(rand.Int63n(maxint64))
-}
-
-func defaultSecureSerialNumber(c *x509.Certificate) error {
+func defaultSerialNumber(c *x509.Certificate) error {
 	if c.SerialNumber != nil {
 		return nil
 	}
 	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
-	serialNumber, err := cryptorand.Int(cryptorand.Reader, serialNumberLimit)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
 	if err != nil {
 		return fmt.Errorf("failed to generate serial number: %w", err)
 	}
