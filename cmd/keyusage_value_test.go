9e616d7f31ddc31fd081ebd6839884e587c13db5
David Schneider <dsbrng25b@gmail.com>
2020-04-21 23:28:31 +0200

    Add tests for key usage value

---
+++ cmd/keyusage_value_test.go
@@ -0,0 +1,67 @@
+package cmd
+
+import (
+	"crypto/x509"
+	"testing"
+)
+
+func Test_keyUsageValueSet(t *testing.T) {
+	var usage x509.KeyUsage
+
+	value := newKeyUsageValue(&usage)
+
+	err := value.Set("CRLSign")
+	if err != nil {
+		t.Errorf("failed to set CRLSign: %w", err)
+		return
+	}
+
+	if usage != x509.KeyUsageCRLSign {
+		t.Errorf("crl sign not set: got: %d, want: %d", value, x509.KeyUsageCRLSign)
+		return
+	}
+
+}
+
+func Test_keyUsageValueSet_multiple(t *testing.T) {
+	var usage x509.KeyUsage
+
+	value := newKeyUsageValue(&usage)
+
+	err := value.Set("CRLSign,CertSign")
+	if err != nil {
+		t.Errorf("failed to set CRLSign,CertSign: %w", err)
+		return
+	}
+
+	want := x509.KeyUsageCRLSign | x509.KeyUsageCertSign
+	if usage != want {
+		t.Errorf("crl sign not set: got: %d, want: %d", usage, want)
+		return
+	}
+
+}
+
+func Test_keyUsageValueSet_multiple_separate(t *testing.T) {
+	var usage x509.KeyUsage
+
+	value := newKeyUsageValue(&usage)
+
+	err := value.Set("CRLSign")
+	if err != nil {
+		t.Errorf("failed to set CRLSign: %w", err)
+		return
+	}
+	err = value.Set("CertSign")
+	if err != nil {
+		t.Errorf("failed to set CertSign: %w", err)
+		return
+	}
+
+	want := x509.KeyUsageCRLSign | x509.KeyUsageCertSign
+	if usage != want {
+		t.Errorf("crl sign not set: got: %d, want: %d", usage, want)
+		return
+	}
+
+}
