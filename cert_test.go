da4a47a31ff652ec36a922b50e1d9fcd24553a44
David Schneider <schneider@puzzle.ch>
2020-07-01 22:25:18 +0200

    Add tests for Create

--- cert_test.go
+++ cert_test.go
@@ -1,27 +1,104 @@
 package pcert
 
 import (
+	"crypto"
 	"crypto/x509"
+	"crypto/x509/pkix"
+	"strings"
 	"testing"
 )
 
+func createAndParse(name string, signCert *x509.Certificate, signKey crypto.PrivateKey) (*x509.Certificate, crypto.PrivateKey, error) {
+	crt := &x509.Certificate{
+		Subject: pkix.Name{
+			CommonName: name,
+		},
+	}
+
+	certPEM, keyPEM, err := Create(crt, signCert, signKey)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	crt, err = Parse(certPEM)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	privKey, err := ParseKey(keyPEM)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return crt, privKey, nil
+}
+
 func TestCreate_selfSigned(t *testing.T) {
-	crt := &x509.Certificate{}
-	crtPEM, keyPEM, err := Create(crt, nil, nil)
+	crt, _, err := createAndParse("My Server", nil, nil)
 	if err != nil {
-		t.Errorf("failed to create self signed certificate: %w", err)
-		return
+		t.Fatal(err)
 	}
 
-	_, err = Parse(crtPEM)
+	if crt.Issuer.CommonName != crt.Subject.CommonName {
+		t.Errorf("issuer and subject common name are not equal: subject=%s issuer=%s", crt.Subject.CommonName, crt.Issuer.CommonName)
+	}
+}
+
+func TestCreate_signed(t *testing.T) {
+	caName := "My CA"
+	serverName := "My Server"
+
+	caCrt, caPrivKey, err := createAndParse(caName, nil, nil)
 	if err != nil {
-		t.Errorf("failed to parse cert PEM: %w", err)
-		return
+		t.Fatal(err)
 	}
 
-	_, err = ParseKey(keyPEM)
+	crt, _, err := createAndParse(serverName, caCrt, caPrivKey)
 	if err != nil {
-		t.Errorf("failed to parse key PEM: %w", err)
-		return
+		t.Fatal(err)
+	}
+
+	if crt.Issuer.CommonName != caCrt.Subject.CommonName {
+		t.Errorf("certificate has wrong issuer: got=%s want=%s", caCrt.Subject.CommonName, crt.Issuer.CommonName)
+	}
+}
+
+func TestCreate_missing_key(t *testing.T) {
+	caName := "My CA"
+	serverName := "My Server"
+
+	caCrt, _, err := createAndParse(caName, nil, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, _, err = createAndParse(serverName, caCrt, nil)
+
+	if err == nil {
+		t.Fatal("no error returned")
+	}
+
+	if !strings.Contains(err.Error(), "key for signing missing") {
+		t.Fatalf("error does not contain string 'key for signing missing': %s", err.Error())
+	}
+}
+
+func TestCreate_missing_certificate(t *testing.T) {
+	caName := "My CA"
+	serverName := "My Server"
+
+	_, caPrivKey, err := createAndParse(caName, nil, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, _, err = createAndParse(serverName, nil, caPrivKey)
+
+	if err == nil {
+		t.Fatal("no error returned")
+	}
+
+	if !strings.Contains(err.Error(), "certificate for signing missing") {
+		t.Fatalf("error does not contain string 'certificate for signing missing': %s", err.Error())
 	}
 }
